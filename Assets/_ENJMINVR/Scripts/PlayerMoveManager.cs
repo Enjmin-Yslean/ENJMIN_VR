615cb2235e3744f95f9d471d1bba0df0e8f1d7bc
JMIN-D1725\enjmin <enjminbonnetedouard@gmail.com>
2017-11-13 18:07:42 +0100

    FIRST

---
+++ Assets/_ENJMINVR/Scripts/PlayerMoveManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PlayerMoveManager : MonoBehaviour {
+
+    private Player m_Player;
+
+    public float m_MovementPadding = 0.3f;
+
+    public float m_Life = 3;
+    public float m_MoveSpeed = 1;
+    public float m_AngleSpeed = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Player = Player.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftYAxis = Input.GetAxis("Left_Y_Axis");
+        float AngleSign = Vector3.Cross(m_Player.transform.forward, m_Player.hmdTransform.forward).y < 0 ? -1 : 1;
+        float angle = AngleSign * Quaternion.Angle(m_Player.transform.rotation, m_Player.hmdTransform.rotation);
+        Debug.Log(leftYAxis);
+        if (Mathf.Abs(leftYAxis) > m_MovementPadding)
+        {
+            Turn(angle);
+            Move(m_Player.transform.forward * leftYAxis, leftYAxis * m_MoveSpeed);
+        }
+    }
+
+    void Move(Vector3 direction, float speed)
+    {
+        m_Player.transform.position += new Vector3 (direction.x, m_Player.transform.position.y, direction.z).normalized * speed * Time.deltaTime;
+    }
+
+    void Turn(float angle)
+    {
+        Quaternion rot = Quaternion.Euler(0, m_Player.hmdTransform.rotation.eulerAngles.y, 0);
+        m_AngleSpeed = Quaternion.Angle(m_Player.transform.rotation, rot);
+        m_Player.transform.rotation = Quaternion.RotateTowards(m_Player.transform.rotation, rot, Time.deltaTime * m_AngleSpeed) ;
+    }
+
+    void OnDrawGizmos()
+    {
+        if(m_Player != null)
+            Gizmos.DrawLine(transform.position, new Vector3(0, m_Player.hmdTransform.rotation.eulerAngles.y, 0));
+    }
+}
---
+++ Assets/_ENJMINVR/Scripts/Zombie.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Zombie : MonoBehaviour {
+
+	public Animator m_Animator;
+
+	public float m_Life = 1;
+	public float m_WalkSpeed = 1;
+	public float m_AttackSpeed = 1;
+
+    public float m_AttackZone = 1.5f;
+
+    private Player m_Player;
+    private float m_DistanceToPlayer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Player = Player.instance;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        m_DistanceToPlayer = Vector3.Distance(transform.position, m_Player.hmdTransform.position);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Attack(Player.instance.hmdTransform);
+        
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+            Move(m_Player.hmdTransform);
+    }
+
+    void Move(Transform target)
+    {
+        StopAllCoroutines();
+        m_Animator.SetTrigger("Move");
+        StartCoroutine(Walk(target));
+    }
+
+   IEnumerator Walk(Transform target)
+    {
+        while (m_DistanceToPlayer > m_AttackZone)
+        {
+            LookAt(target);
+            transform.localPosition += transform.forward * m_WalkSpeed * Time.deltaTime;
+            
+            yield return null;
+        }
+
+        Attack(Player.instance.hmdTransform);
+        yield return null;
+    }
+
+    void Attack(Transform target)
+    {
+        StopAllCoroutines();
+        LookAt(target);
+        m_Animator.SetTrigger("Attack");
+    }
+
+    void LookAt(Transform target)
+    {
+        transform.LookAt(new Vector3(target.position.x, transform.localPosition.y, target.position.z));
+    }
+
+
+	#region ZombieAnimation
+
+	public void EndAttack()
+	{
+        m_Animator.SetTrigger("Idle");
+	}
+	#endregion
+}
