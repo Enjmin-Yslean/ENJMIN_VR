d79286231f36a3a7db31d6c6c2e90b5dc9376f84
JMIN-D1725\enjmin <enjminbonnetedouard@gmail.com>
2017-11-16 14:20:12 +0100

    MOD : Improvements

--- Assets/_ENJMINVR/Scripts/UI_Player.cs
+++ Assets/_ENJMINVR/Scripts/UI_Player.cs
@@ -7,22 +7,29 @@ public class UI_Player : MonoBehaviour {
 
     public Image coeur;
     public GameObject panel;
-    public int m_NbHeart;
+    private int m_NbHeart;
+    public Text m_FPSText;
 
-    private int actual;
     private List<Image> coeurs = new List<Image>();
 
 	// Use this for initialization
 	void Start ()
     {
         //Reset_UI();
+        m_FPSText.gameObject.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            m_FPSText.gameObject.SetActive(!m_FPSText.gameObject.activeInHierarchy);
+        }
+        if (m_FPSText.gameObject.activeInHierarchy && Time.frameCount % 45 == 0)
+            FPSCounter();
+
+    }
 
     public void PerteCoeur(int newHeartNb)
     { 
@@ -49,5 +56,11 @@ public class UI_Player : MonoBehaviour {
         {
             coeurs.Add(Instantiate(coeur, panel.transform));
         }
+    }
+
+    void FPSCounter()
+    {
+        float fps = Mathf.Round(1 / Time.deltaTime);
+        m_FPSText.text = "FPS : " + fps;
     }
 }
